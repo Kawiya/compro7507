706c29bd4d71dda76409b07f204ccd56669c150d
Kawiya <doy_kawiya@hotmail.com>
2012-07-14 23:48:36 +0700

    Chapter 3 : Statements and Decisions

---
+++ src/th/ac/nu/students/u54347507/compro/ch03/LabelStatement.java
@@ -0,0 +1,11 @@
+ 
+package th.ac.nu.students.u54347507.compro.ch03;
+ 
+public class LabelStatement {
+    public static void main(String[] args) {
+    first : {
+        System.out.print("Hello, ");
+        here : System.out.println("World!");
+         }
+    }
+}
